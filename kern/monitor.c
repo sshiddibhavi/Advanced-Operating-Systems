05acd7df8279958f26b5a030f5458daf50e6efa6
sshiddibhavi <sshiddib@uncc.edu>
2017-02-09 10:51:08 -0500

    changes to lab2 after handin

--- kern/monitor.c
+++ kern/monitor.c
@@ -24,6 +24,7 @@ struct Command {
 static struct Command commands[] = {
 	{ "help", "Display this list of commands", mon_help },
 	{ "kerninfo", "Display information about the kernel", mon_kerninfo },
+	{ "backtrace", "Display information on backtrace", mon_backtrace },
 };
 
 /***** Implementations of basic kernel monitor commands *****/
@@ -57,7 +58,16 @@ mon_kerninfo(int argc, char **argv, struct Trapframe *tf)
 int
 mon_backtrace(int argc, char **argv, struct Trapframe *tf)
 {
-	// Your code here.
+	uint32_t *x;
+	struct Eipdebuginfo info;
+	x=(uint32_t *)read_ebp();
+	while(x)
+	{
+		cprintf("ebp %08x eip %08x args %08x %08x %08x %08x %08x\n",x,x[1],x[2],x[3],x[4],x[5],x[6]);		
+		debuginfo_eip(x[1],&info);
+		cprintf("%s:%d: %.*s+%d %d\n",info.eip_file,info.eip_line,info.eip_fn_namelen,info.eip_fn_name,(x[1] - info.eip_fn_addr),info.eip_fn_narg);
+		x=(uint32_t *)x[0];
+	}
 	return 0;
 }
 
